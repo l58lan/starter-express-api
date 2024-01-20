350dfa38b58dbc273040d119ebccc89b9eaefc97
l58lan <157161523+l58lan@users.noreply.github.com>
2024-01-20 04:34:26 +0300

    nver

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 import path from "path";
 
 const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 3000 } = process.env;
-const base = "https://dress-goshawk.cyclic.app";
+const base = "https://api-m.paypal.com";
 const app = express();
 
 // host static files
