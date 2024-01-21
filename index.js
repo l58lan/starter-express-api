5e6d05566a798fd80ed6f6c409d291f6003c3c2e
l58lan <157161523+l58lan@users.noreply.github.com>
2024-01-21 17:40:52 +0300

    345

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/create_order', (req, res) => {
                 'purchase_units': [{
                     'amount': {
                         'currency_code': 'USD',
-                        'value': '85.00'
+                        'value': '5.00'
                     }
                 }]
             };
