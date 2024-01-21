a35ccf2eea0d31d5c5cec27676a6d57d559371a8
l58lan <157161523+l58lan@users.noreply.github.com>
2024-01-21 16:41:51 +0300

    new

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 8888;
 const environment = process.env.ENVIRONMENT || 'production';
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
-const endpoint_url = 'https://api-m.paypal.com';
+const endpoint_url = 'https://api.paypal.com';
 
 /**
  * Creates an order and returns it as a JSON response.
@@ -180,6 +180,3 @@ function get_access_token() {
         })
 }
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
-})
